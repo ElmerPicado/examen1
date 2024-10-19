49bab098a4829346fa34820fe9c5d371b3704a9b
ElmerPicado <epicadomiranda@gmail.com>
2024-10-18 21:44:49 -0600

    examen1 progra2

---
+++ CONTROL_RH/Empleado.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Server;
+
+namespace CONTROL_RH
+{
+    public static class Empleado
+    {
+        public static int    [] id = new int [10];
+        public static string [] cedula = new string[10];
+        public static string [] nombre = new string[10];
+        public static string [] direccion = new string[10]; 
+        public static string [] telefono = new string[10];
+        public static float [] salario = new float[10];
+
+        public static void listaNombres()
+        
+        {
+            for (int i = 1; i <= id.Length; i++) 
+            
+            {
+                Console.WriteLine($"nombre : {Empleado.nombre[i]}");
+
+
+            }
+        
+         
+        
+        }
+        public static void calculo() 
+        {
+            float sumaSalarios = 0;
+            int contadorEmpleados = 0;
+
+            for (int i = 0; i < Empleado.salario.Length; i++)
+            {
+                if (Empleado.salario[i] > 0)
+                {
+                    sumaSalarios += Empleado.salario[i];
+                    contadorEmpleados++;
+                }
+            }
+
+            if (contadorEmpleados > 0)
+            {
+                float promedio = sumaSalarios / contadorEmpleados;
+                Console.WriteLine($"El promedio de los salarios es: {promedio}");
+            }
+            else
+            {
+                Console.WriteLine("No hay empleados registrados con salario.");
+            }
+
+        }
+        
+        
+        
+        
+        
+        
+        public static void agregar() {
+
+            int indice = 0;
+            int num = 1;
+
+            while (indice < Empleado.id.Length)
+            {
+
+                Console.Clear();
+                Console.WriteLine($"***REGISTRO empleados {num}***");
+                Console.Write($"Digite su ID: ");
+                Empleado.cedula[indice] = Console.ReadLine();
+
+                Console.Write(" nombre del Empleado: ");
+                Empleado.nombre[indice] = (Console.ReadLine());
+
+                Console.Write($"Direccion: ");
+                Empleado.direccion[indice] = Console.ReadLine();
+
+                Console.Write($"telefono: ");
+                Empleado.telefono[indice] = Console.ReadLine();
+
+                Console.Write($"Salario : ");
+                Empleado.salario[indice] = int.Parse(Console.ReadLine());
+
+                num++;
+                indice++;
+                Console.Clear();
+            }
+            Console.WriteLine("Los empleados han sido agregados \n");
+        }
+
+        public static void modificarEmpleado()
+        {
+            Console.Clear();
+            Console.Write("Digite la cedula del empleado:");
+
+            string NewName = Console.ReadLine();
+
+            for (int i = 0; i < Empleado.id.Length; i++)
+            {
+                if (Empleado.cedula[i] == NewName)
+                {
+                    Console.Clear();
+                    Console.Write("*****Datos del  Empleado:****\n");
+                    Console.WriteLine($"Cedula: {Empleado.cedula[i]} Nombre Art.: {Empleado.nombre[i]} ");
+
+                    Console.WriteLine($"Digite  un nuevo nombre ");
+                    Empleado.nombre[i] = Console.ReadLine();
+
+                    Console.WriteLine($"Digite  una nueva direccion ");
+                    Empleado.direccion[i] = Console.ReadLine();
+
+                    Console.WriteLine($"Digite  un nuevo telefono ");
+                    Empleado.telefono[i] = Console.ReadLine();
+
+                    Console.WriteLine($"Digite  Salario ");
+                    Empleado.salario[i] = float.Parse(Console.ReadLine());
+
+
+
+                    break;
+                }
+            }
+        }
+        public static void consultar()
+        {
+
+            string entrada = "";
+            Console.Clear();
+            Console.WriteLine("***Buscador de Empleados***\n");
+            Console.Write("ingrese el Nombre o cedula del Empleado: ");
+            entrada = Console.ReadLine();
+            if (int.TryParse(entrada, out int result))
+            {
+                for (int i = 0; i < Empleado.id.Length; i++)
+                {
+
+                    if (entrada == Empleado.nombre[i] || entrada == Empleado.cedula[i])
+                    {
+                        Menu.Impresion_datos(i);
+
+                    }
+                }
+            }
+
+
+        }  
+        public static void BorrarEmpleado()
+        {
+            char sino = ' ';
+            Console.Clear();
+            Console.Write("Digite el codigo del articulo:");
+            int Code = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < Empleado.id.Length; i++)
+            {
+                if (Empleado.id[i] == Code)
+                {
+                    Console.Clear();
+                    Console.Write("*****Datos del  articulo:****\n");
+                    Menu.Impresion_datos(i);
+                    Console.WriteLine($"esta seguro que lo quiere borrar ? \nDigite 'S' para si o 'N'para no");
+                    sino = char.Parse(Console.ReadLine().ToLower());
+                    if (sino == 's')
+                    {
+                        Empleado.nombre[i] = " ";
+                        Empleado.cedula[i] = "";
+                        Empleado.salario[i] = 0;
+                        Empleado.telefono[i] = "";
+                        Empleado.direccion[i] = "";
+                        Console.WriteLine("***El empleado a sido Borrado***\n");
+
+                    }
+                    else { break; }
+                }
+            }
+        }
+        public static void inicializar()
+        {
+            for (int i = 0; i < Empleado.id.Length; i++)
+            {
+                Empleado.nombre[i] = " ";
+                Empleado.cedula[i] = "";
+                Empleado.salario[i] = 0;
+                Empleado.telefono[i] = "";
+                Empleado.direccion[i] = "";
+            }
+
+            Console.Clear();
+            Console.WriteLine("***El sistema ha sido inicializados**\n");
+        }
+        public  static void reportes()
+
+        {
+            int op = 0;
+
+            do
+            {
+                Console.WriteLine("** subMenu reporte empleados **\n");
+                Console.WriteLine("1- Listar todos los empleados");
+                Console.WriteLine("2- Calcular promedio salarios");
+                op = Console.Read();
+                switch (op)
+
+                {
+                    case 1:
+                        listaNombres(); break;
+
+                        case 2:
+                        calculo(); break;
+
+
+                    
+
+
+
+                }
+
+
+
+            } while (false);
+
+
+
+        }
+    }
+}
---
+++ CONTROL_RH/Menu.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CONTROL_RH
+{
+    internal class Menu
+    {
+        public static void Impresion_datos(int i)
+        {
+            Console.WriteLine($"== cedula: {Empleado.cedula[i]}/\n");
+            Console.WriteLine($"nombre : {Empleado.nombre[i]} / direccion : {Empleado.direccion[i]} / telefono: {Empleado.telefono[i]} " +
+                $"/ salario: {Empleado.salario[i]} ");
+            Console.WriteLine("-------------------------------------------------------------------\n");
+
+        }
+        public static  void principalmenu() { 
+            
+        int opcion = 0;
+
+            do
+            {
+                Console.WriteLine("**Menu principal RH **\n");
+                Console.WriteLine("1- Agrgar empleados");
+                Console.WriteLine("2- Consultar Empleados");
+                Console.WriteLine("3- Modificar Empleados");
+                Console.WriteLine("4- Borrar Empleados");
+                Console.WriteLine("5- Inicializar Arreglos");
+                Console.WriteLine("6-reportes");
+                Console.WriteLine("7- Salir\n");
+                Console.WriteLine("Digite una opcion:");
+                opcion = int.Parse(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        Empleado.agregar(); break;
+                    case 2: Empleado.consultar(); break;
+                    case 3: Empleado.modificarEmpleado(); break;
+                    case 4: Empleado.BorrarEmpleado(); break;
+                    case 5: Empleado.inicializar(); break;
+                    case 6: Empleado.reportes(); break;
+                    case 7: Console.WriteLine("Saliendo del Sistema"); break;
+                    default: Console.WriteLine("***Opcion Incorrecta***"); break;
+
+
+                }
+
+
+
+            } while (opcion != 7); 
+
+
+
+        }
+
+      
+
+           
+
+
+        
+      
+
+        
+        
+        
+
+        
+
+        
+
+    }
+}
